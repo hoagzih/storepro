43e265ade2476f0425eafb8271b62e5f7ea85912
Hoàng Vinh <comvia80@gmail.com>
2025-11-14 21:30:16 +0700

    rr

--- products.js
+++ products.js
@@ -4,7 +4,7 @@ window.PRODUCTS_DATA = [
 	{
 		id: 1,
 		name: "Gemini Pro (12 Tháng)",
-		type: "Tài khoản Cao cấp",
+		type: "Tài khoản Cao cấp, Dịch vụ Nâng cấp",
 		price: "99.000 VNĐ",
 		description: "Gemini Pro là một mô hình AI tiên tiến của Google, nổi bật nhờ khả năng đa phương thức, cho phép nó hiểu và xử lý đồng thời văn bản, hình ảnh, âm thanh, video và mã lập trình. Mô hình này rất mạnh mẽ trong việc suy luận phức tạp, giải quyết các vấnd đề khó về logic và lập trình. Với bộ nhớ lớn, nó có thể phân tích lượng thông tin khổng lồ như toàn bộ cuốn sách hoặc cơ sở mã..",
 		details: "Bảo hành trong suốt quá trình sử dụng. \nKhi mua, bạn sẽ được cấp tài khoản và mật khẩu đầy đủ. \nHoạt động trên mọi thiết bị.",
@@ -13,7 +13,7 @@ window.PRODUCTS_DATA = [
     {
 		id: 2,
 		name: "Spotify Premium (2 Tháng)",
-		type: "Tài khoản Cao cấp",
+		type: "Tài khoản Cao cấp, Dịch vụ Nâng cấp",
 		price: "49.000 VNĐ",
 		description: "Nghe nhạc chất lượng cao không quảng cáo, phát bất kỳ bài hát nào bạn muốn, tải nhạc về nghe offline.",
 		details: "Bảo hành trong suốt quá trình sử dụng. \nKhi mua, bạn sẽ được cấp tài khoản và mật khẩu đầy đủ. \nHoạt động trên mọi thiết bị.",
@@ -22,7 +22,7 @@ window.PRODUCTS_DATA = [
     {
 		id: 3, // <-- SẢN PHẨM MỚI THÊM VÀO
 		name: "YouTube Premium (1 Tháng)",
-		type: "Tài khoản Cao cấp",
+		type: "Tài khoản Cao cấp, Dịch vụ Nâng cấp",
 		price: "69.000 VNĐ",
 		description: "Xem video không quảng cáo, phát trong nền và tải video về xem offline. Bao gồm cả YouTube Music Premium.",
 		details: "Bảo hành trong suốt quá trình sử dụng. \nKhi mua, bạn sẽ được cấp tài khoản và mật khẩu đầy đủ. \nHoạt động trên mọi thiết bị.",
